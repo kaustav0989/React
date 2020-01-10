90cfb6204b6f6d99dd138c18c0c5ac21f601710a
Kaustav Paul <roni0989@gmail.com>
2020-01-11 00:05:56 +0530

    master

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -5,12 +5,14 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
-    <Message />  
-    {/*<Greet name="Subham" heroName="Batman">
+    <Counter />
+    {/*<Message />  
+    <Greet name="Subham" heroName="Batman">
         <p>He is a good man</p>
       </Greet>
       <Greet name="Kaustav" heroName="Hero Alam"/>

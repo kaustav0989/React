f731666950f782dbc484ce1467be7fcc5b0af4d6
Kaustav Paul <roni0989@gmail.com>
2020-01-19 00:08:15 +0530

    commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -10,13 +10,15 @@ import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
 import EventBind from './components/EventBind'
 import ParentComponent from './components/ParentComponent'
-import UserGreeting from './components/UserGreeting';
+import UserGreeting from './components/UserGreeting'
+import NameList from './components/NameList';
 
 function App() {
   return (
     <div className="App">
-    <UserGreeting />
-    {/*<ParentComponent />
+    <NameList />
+    {/*<UserGreeting />
+    <ParentComponent />
     <EventBind />
     <ClassClick />
     <FunctionClick />
---
+++ hello-world/src/components/NameList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Person from './Person'
+
+function NameList() {
+    /* const names = ['Subham','Kaustav']
+    const nameList = names.map( name => <h2>{name}</h2> ) 
+    return <div>{nameList}</div> */
+
+    const persons = [
+        {
+            aka : 'Marine',
+            name : 'Subham',
+            skill : 'python'
+        },
+        {
+            aka : 'Marcos',
+            name : 'Kaustav',
+            skill : 'varendabhaja'
+        }
+    ]
+
+    //We need to pass unique key props for every element of the list, key props can't be rendered in the child component
+    const personList = persons.map( person => <Person key={person.aka} person ={person} /> )
+    return <div>{personList}</div>
+}
+
+export default NameList

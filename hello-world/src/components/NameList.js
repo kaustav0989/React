9892510f09feaf6bfec00d66cc31e40ca5ebbfce
Kaustav Paul <roni0989@gmail.com>
2020-01-19 00:42:17 +0530

    commit

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
-    /* const names = ['Subham','Kaustav']
-    const nameList = names.map( name => <h2>{name}</h2> ) 
-    return <div>{nameList}</div> */
+    const names = ['Subham','Kaustav','Subham','Kaustav']
+    const nameList = names.map( (name , index) => <h2 key={index}>{index} {name}</h2> ) 
+    return <div>{nameList}</div>
 
-    const persons = [
+    /* const persons = [
         {
             aka : 'Marine',
             name : 'Subham',
@@ -21,7 +21,7 @@ function NameList() {
 
     //We need to pass unique key props for every element of the list, key props can't be rendered in the child component
     const personList = persons.map( person => <Person key={person.aka} person ={person} /> )
-    return <div>{personList}</div>
+    return <div>{personList}</div> */
 }
 
 export default NameList
